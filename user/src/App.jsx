9ca31c35e6df73836044615dd7481b16ff510400
KavinduGD <it21038914@my.sliit.lk>
2023-11-29 23:12:56 +0530

    login complete

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/homepage";
 import RoadRoutes from "./pages/routes";
@@ -6,20 +6,40 @@ import Journey from "./pages/journey";
 import Booking from "./pages/booking";
 import Navbar from "./component/navbar";
 import Footer from "./component/footer";
+import Login from "./pages/loginpage";
+
+const user = null;
+
+const ProtectedRoute = ({ children }) => {
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
 
 function App() {
   return (
     <div className="app flex flex-col overflow-y-scroll min-h-screen">
-      <Navbar />
-      <div className="w-full h-full flex-1 ">
+      <div className={`${!user} ? hidden : ""`}>
+        <Navbar />
+      </div>
+      <div className="w-full h-full flex-1  ">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/routes" element={<RoadRoutes />} />
           <Route path="journey/" element={<Journey />} />
           <Route path="/booking" element={<Booking />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
-      <div className="mt-auto">
+      <div className={`${!user} ? hidden : "mt-auto"`}>
         <Footer />
       </div>
     </div>
---
+++ user/src/pages/loginpage/index.jsx
@@ -0,0 +1,112 @@
+import { Checkbox } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useState } from "react";
+function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="flex h-screen w-full font-roboto">
+      <div className="w-[34%] flex items-center flex-col">
+        <div className="flex flex-col items-center">
+          <img
+            src="https://res.cloudinary.com/dnoobzfxo/image/upload/v1701275328/Kotelawala_Defence_University_crest-removebg-preview_1_qprg9r.png"
+            alt=""
+            className="w-[210px] mt-[10%]"
+          />
+          <div className="font-roboto_slab text-[30px] font-bold flex flex-col items-center leading-[92%]">
+            <p>General</p>
+            <p>Sir John Kotelawala</p>
+            <p>Defence University</p>
+            <p>Transport Management System</p>
+          </div>
+        </div>
+        <div className="mt-[40px] w-full items-center">
+          <form action="">
+            <div className="flex flex-col items-center">
+              <div className="w-[75%]">
+                <label
+                  htmlFor="email"
+                  className="font-semibold text-[#383838] "
+                >
+                  University Email Address
+                </label>
+                <input
+                  id="email"
+                  type="text"
+                  className="w-full bg-red-100 h-[56px] pl-[20px] py-[7px] font-normal text-sm text-[#515151]"
+                  style={{
+                    borderRadius: "7.979px",
+                    border: "1px solid #747474",
+                    background: "#FFF",
+                  }}
+                  placeholder="Enter your email address"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col items-center mt-[12px]">
+              <div className="w-[75%] relative">
+                <label
+                  htmlFor="password"
+                  className="font-semibold text-[#383838]"
+                >
+                  Password
+                </label>
+                <input
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  className="w-full bg-red-100 h-[56px] pl-[20px] py-[7px] font-normal text-sm text-[#515151]"
+                  style={{
+                    borderRadius: "7.979px",
+                    border: "1px solid #747474",
+                    background: "#FFF",
+                  }}
+                  placeholder="Enter your password"
+                />
+                <VisibilityIcon
+                  className="absolute top-[40px] right-4"
+                  fontSize="medium"
+                  color={showPassword ? "inherit" : "disabled"}
+                  onClick={() => {
+                    setShowPassword((pre) => !pre);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="w-[75%] flex justify-between items-center">
+                <div>
+                  <Checkbox />
+                  <span className="text-gray-400 text-base font-medium">
+                    Remember me
+                  </span>
+                </div>
+
+                <div className="text-red-500 text-[15px] font-normal">
+                  Reset Password
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center mt-[15px]">
+              <div className="w-[75%] ">
+                <button
+                  type="submit"
+                  className="w-full text-white bg-main_blue h-[60px] rounded-lg text-[20px] font-medium"
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div
+        className="flex-1 bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage:
+            "url(https://res.cloudinary.com/dnoobzfxo/image/upload/v1701274834/bg-girl_qwuwun.png)",
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default Login;

fb750ddaf64268485f8cfbb706937f46c5fea64c
KavinduGD <it21038914@my.sliit.lk>
2023-11-29 07:21:10 +0530

    map added

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./component/Navbar";
+import Map from "./pages/Map";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <div>sdsds</div>
+      <Map />
     </div>
   );
 }

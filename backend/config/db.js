29fae348cd4dd385b8ae92791027048adb37d257
KavinduGD <it21038914@my.sliit.lk>
2023-11-29 15:10:12 +0530

    db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
 
+const   MONGO_USERNAME = 'admin';
+const   MONGO_PASSWORD = 'admin';
+
+const MONGO_HOSTNAME = '
+mongodb://127.0.0.1:27017/test';
+
 const connect = () => {
-  return mongoose.connect("mongodb://127.0.0.1:27017/file-upload");
-};
+    mongoose.connect(MONGO_HOSTNAME, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        auth: {
+        user: MONGO_USERNAME,
+        password: MONGO_PASSWORD
+        }
+    });
+    mongoose.connection.on("error", err => {
+        console.log(err);
+        console.log(
+        "MongoDB connection error. Please make sure MongoDB is running."
+        );
+        process.exit();
+    });
+    }

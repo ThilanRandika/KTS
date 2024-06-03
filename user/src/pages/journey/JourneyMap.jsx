415cff654202f49fc30ef46b42055d057bd55514
KavinduGD <it21038914@my.sliit.lk>
2024-06-03 15:14:51 +0530

    center changed

--- user/src/pages/journey/JourneyMap.jsx
+++ user/src/pages/journey/JourneyMap.jsx
@@ -15,7 +15,7 @@ function JourneyMap({ googleRoutes, stations, startLocation }) {
   const mapRef = useRef();
 
   const center = useMemo(
-    () => ({ lat: 6.833813409471106, lng: 79.88634319394335 }),
+    () => ({ lat:53.79377638417939, lng: -1.7563649007954596 }),
     []
   );
 
